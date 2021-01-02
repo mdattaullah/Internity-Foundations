31ea134a27d216f8408ecd1cca9fb84649f84bca
Riya Jain <55906793+rjrealworld@users.noreply.github.com>
2021-01-02 20:40:46 +0530

    Update Import CSV From GitHub.py

--- Day 4+5/Import CSV From GitHub.py
+++ Day 4+5/Import CSV From GitHub.py
@@ -1,5 +1,5 @@
 import pandas as pd
-url = 'https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes/blob/master/all/all.csv?raw=true'
-df = pd.read_csv(url,index_col=0)
+url = 'https://raw.githubusercontent.com/rjrealworld/Internity-Foundations/main/Day%204%2B5/Intra-StateWarData_v4.1.csv'
+df = pd.read_csv(url,index_col=0, encoding='latin-1')
 
 print(df.head(5))
